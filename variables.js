e760efef99e3e808b61501bb7ceb76bfb9a28dcd
Rory Magee <roryemagee@gmail.com>
2021-10-27 22:26:34 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var famousPerson = "Ryan Gosling";
+var isActive = true;
+famousPerson = "Morgan Freeman";
+var num3 = 3;
+var num9 = 9;
+var sum = num3 + num9;
+var animals = ["Lions", "Tigers", "Bears"];
+animals.splice(0, 1);
+console.log(famousPerson.length);
