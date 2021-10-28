d52c56557fb6d362ddd02d26de2bc1e1ccb29cfb
Rory Magee <roryemagee@gmail.com>
2021-10-27 22:38:35 -0600

    Add modelB

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var car = {
+  brand: "Tesla",
+  model: "X",
+  year: 2017,
+  batteryPower: 100,
+  isCool: true,
+  isNew: true,
+  seatOccupants: ["Rory", "Rocky"],
+  seatAngles: [135, 135],
+}
