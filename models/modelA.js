3e1297bb330bd58070d27d39235ea6cc23102720
Rory Magee <roryemagee@gmail.com>
2021-10-27 22:33:03 -0600

    Add modelA

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airport = {
+  name: "Santa Ana International Airport",
+  nickName: "John Wayne",
+  maxOccupancy: 500,
+  terminals: 3,
+  greatAirport: true,
+  open: true,
+  recentPassengers: ["Billy", "Bob", "Jim"],
+  passengerAges: [35, 72, 19],
+}
